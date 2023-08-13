f745c225682534671a23142f13d4477d2383de15
CharlesOkechukwu <realcharlieok@gmail.com>
2023-08-13 06:54:56 -0700

    print element at an index in a list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,6 +3,6 @@ def element_at(my_list, idx):
     """
     print element at an indx
     """
-    if (idx >= len(my_list)):
+    if (idx >= len(my_list) or idx < 0):
         return None
     return my_list[idx]
