b78875db6f801c992cfe05b0202bab79bb4a977e
CharlesOkechukwu <realcharlieok@gmail.com>
2023-08-14 16:04:03 -0700

    add two tuples

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    """
+    add tupules together
+    """
+    a_len = len(tuple_a)
+    b_len = len(tuple_b)
+    if (a_len >= 2) and (b_len >= 2):
+        return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    elif (a_len >= 2) and (b_len < 2):
+        if (b_len <= 0):
+            return tuple_a
+        else:
+            return (tuple_a[0] + tuple_b[0], tuple_a[1] + 0)
+    elif (a_len < 2) and (b_len >= 2):
+        if (a_len <= 0):
+            return tuple_b
+        else:
+            return (tuple_a[0] + tuple_b[0], 0 + tuple_b[1])
+    else:
+        if (len_a <= 0) and (len_b <= 2):
+            return tuple_b
+        elif (len_b <= 0) and (len_a <= 2):
+            return tuple_a
+        else:
+            return (tuple_a[0] + tuple_b[0], 0 + 0)
