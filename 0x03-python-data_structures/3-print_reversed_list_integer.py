e3f94a1219bae4ecb6414ac701db4018331ebf06
CharlesOkechukwu <realcharlieok@gmail.com>
2023-08-13 22:57:46 -0700

    print list in reverse order

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -4,7 +4,7 @@ def print_reversed_list_integer(my_list=[]):
     print list in reverse order
     """
     list_len = len(my_list) - 1
-    if (list_len > 0):
+    if (my_list):
         while (list_len >= 0):
             print("{:d}".format(my_list[list_len]))
             list_len -= 1
