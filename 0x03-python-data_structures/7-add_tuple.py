5bccb795734ba5fdf22113e9f85a6f1514af5d5d
CharlesOkechukwu <realcharlieok@gmail.com>
2023-08-14 17:16:17 -0700

    add two tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,24 +5,16 @@ def add_tuple(tuple_a=(), tuple_b=()):
     """
     a_len = len(tuple_a)
     b_len = len(tuple_b)
-    if (a_len >= 2) and (b_len >= 2):
-        return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-    elif (a_len >= 2) and (b_len < 2):
-        if (b_len <= 0):
-            return tuple_a
-        else:
-            return (tuple_a[0] + tuple_b[0], tuple_a[1] + 0)
-    elif (a_len < 2) and (b_len >= 2):
-        if (a_len <= 0):
-            return tuple_b
-        else:
-            return (tuple_a[0] + tuple_b[0], 0 + tuple_b[1])
+    if (a_len < 2) and (a_len > 0):
+        tuple_a = (tuple_a[0], 0)
+    elif (a_len <= 0):
+        tuple_a = (0, 0)
     else:
-        if (len_a <= 0) and (len_b <= 2):
-            return tuple_b
-        elif (len_b <= 0) and (len_a <= 2):
-            return tuple_a
-        elif (len_b <= 0) and (len_a <= 0):
-            return (0, 0)
-        else:
-            return (tuple_a[0] + tuple_b[0], 0 + 0)
+        tuple_a = (tuple_a[0], tuple_a[1])
+    if (b_len < 2) and (b_len > 0):
+        tuple_b = (tuple_b[0], 0)
+    elif (b_len <= 0):
+        tuple_b = (0, 0)
+    else:
+        tuple_b = (tuple_b[0], tuple_b[1])
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
