1cfeed3f0075fdd2d9ead4b1e0be631f1dc4a7aa
CharlesOkechukwu <realcharlieok@gmail.com>
2023-08-16 12:08:10 -0700

    add key/value to a dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    """
+    update value or key and value in a dictionary
+    """
+    a_dictionary[key] = value
+    return a_dictionary
