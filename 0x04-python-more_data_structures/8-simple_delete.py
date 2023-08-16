1b3f7cd6f91455b5e8e0eb78d05f51562be83f9e
CharlesOkechukwu <realcharlieok@gmail.com>
2023-08-16 12:22:01 -0700

    delete a key/value

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def simple_delete(a_dictionary, key=""):
+    """
+    delete a key/value in a dictionary
+    """
+    if (key in a_dictionary):
+        del(a_dictionary[key])
+        return a_dictionary
+    else:
+        return a_dictionary
