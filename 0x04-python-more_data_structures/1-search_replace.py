d2a0cb18fad2a1625a36256b2d639d9dfd654358
CharlesOkechukwu <realcharlieok@gmail.com>
2023-08-16 09:05:10 -0700

    search and replace an element in a list

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    if (my_list):
+        new = []
+        for i in my_list:
+            if (i == search):
+                new.append(replace)
+            else:
+                new.append(i)
+        return new
