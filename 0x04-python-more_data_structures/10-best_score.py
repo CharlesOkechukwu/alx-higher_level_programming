dde8bff4868bb6362da20fe67dc848a9d7a7f3bd
CharlesOkechukwu <realcharlieok@gmail.com>
2023-08-16 14:01:46 -0700

    return key with highest value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,13 +3,12 @@ def best_score(a_dictionary):
     """
     return largest value in a dictionary
     """
-    if (a_dictionary):
-        keys = list(a_dictionary.keys())
-        num = a_dictionary[keys[0]]
-        for k in keys:
-            if (a_dictionary[k] > num):
-                num = a_dictionary[k]
-                key = k
-        return key
-    else:
+    if a_dictionary is None:
         return None
+    keys = list(a_dictionary.keys())
+    num = a_dictionary[keys[0]]
+    for k in keys:
+        if (a_dictionary[k] > num):
+            num = a_dictionary[k]
+            key = k
+    return key
