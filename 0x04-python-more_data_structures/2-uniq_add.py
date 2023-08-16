6068a17d0594888699899eb2fa26a08b8daaddfb
CharlesOkechukwu <realcharlieok@gmail.com>
2023-08-16 09:29:09 -0700

    add unique numbers

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    if (my_list):
-        num = 0
-        new = set(my_list)
-        for i in new:
-            num += i
-        return num
+    """
+    add unique numbers in a list
+    """
+    num = 0
+    new = set(my_list)
+    for i in new:
+        num += i
+    return num
