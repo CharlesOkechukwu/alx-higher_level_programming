2372dab21f6e41e362339d015c1f773a242fddaf
CharlesOkechukwu <realcharlieok@gmail.com>
2023-09-06 09:09:15 -0700

    add method to calculate perimeter and area of Rectangle class

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""A class that defines a rectangle
+an empty class
+"""
+
+
+class Rectangle:
+    """Rectangle class
+    contains an empty class
+    """
+    def __init__(self, width=0, height=0):
+        """initiate attributes for Rectangle class"""
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        """retrieve width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """set value of width"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """height property getter"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """height value setter function"""
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        """calculates and returns area of a rectangle"""
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """calculate and return the perimeter of a rectangle"""
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        else:
+            peri = (self.__height + self.__width) * 2
+            return peri
