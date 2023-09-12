4ed9133828b450cabdc7568e1f3a7ab294f1fbe4
CharlesOkechukwu <realcharlieok@gmail.com>
2023-09-11 17:54:18 -0700

    create square a subclass of Rectangle and BaseGeometry

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module creates a class square
+which is a subclass of the class
+Rectangle and inherits its attributes
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square is a subclass of Rectangle
+    inherits all the arrtributes of Rectangle and
+    BaseGeometry
+    """
+    def __init__(self, size):
+        """initialize size of square"""
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
