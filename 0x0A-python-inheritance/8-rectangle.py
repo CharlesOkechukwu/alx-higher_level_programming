19da7249f00dafb00482aa1e7c47a67210efd8ce
CharlesOkechukwu <realcharlieok@gmail.com>
2023-09-11 17:19:15 -0700

    create a subclass that inherits from BaseGeometry

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""Module is a class that inherits
+form BaseGeometry
+"""
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class is a subclass of
+    BaseGeometry, with its own private variables
+    """
+    def __init__(self, width, height):
+        """Initialize private attributes"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
