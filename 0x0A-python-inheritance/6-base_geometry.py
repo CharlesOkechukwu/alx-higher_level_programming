45c7992617bf30e18480f6d0e790c0f1fb60c7c6
CharlesOkechukwu <realcharlieok@gmail.com>
2023-10-09 07:48:23 -0700

    add area attribute to geometry class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module for BaseGeometry class
+currently empty but would add attributes soon
+"""
+
+
+class BaseGeometry(object):
+    """A class for the BaseGeometry
+    object contains attributes and
+    methods
+    """
+    def area(self):
+        """calculates the area of two objects"""
+        raise Exception("area() is not implemented")
