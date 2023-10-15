88e611b2ee848c375c3a144a6d8f25490cd1f704
CharlesOkechukwu <realcharlieok@gmail.com>
2023-10-15 03:25:38 -0700

    unittest for Base class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -22,42 +22,58 @@ class Rectangle(Base):
         self.y = y
         super().__init__(id)
 
-        @property
-        def width(self):
-            """getter of private class attribute width"""
-            return self.__width
+    @property
+    def width(self):
+        """getter of private class attribute width"""
+        return self.__width
 
-        @width.setter
-        def width(self, value):
-            """setter of private attribute width"""
-            self.__width = value
+    @width.setter
+    def width(self, value):
+        """setter of private attribute width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
 
-        @property
-        def height(self):
-            """getter of private attribute height"""
-            return self.__height
+    @property
+    def height(self):
+        """getter of private attribute height"""
+        return self.__height
 
-        @height.setter
-        def height(self, value):
-            """setter of height"""
-            self.__height = value
+    @height.setter
+    def height(self, value):
+        """setter of height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
 
-        @property
-        def x(self):
-            """getter of private attribute x"""
-            return self.__x
+    @property
+    def x(self):
+        """getter of private attribute x"""
+        return self.__x
 
-        @x.setter
-        def x(self, value):
-            """set value of private attribute x"""
-            self.__x = value
+    @x.setter
+    def x(self, value):
+        """set value of private attribute x"""
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        elif value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
 
-        @property
-        def y(self):
-            """getter of private attribute y"""
-            return self.__y
+    @property
+    def y(self):
+        """getter of private attribute y"""
+        return self.__y
 
-        @y.setter
-        def y(self, value):
-            """set the value of __y"""
-            self.__y = value
+    @y.setter
+    def y(self, value):
+        """set the value of __y"""
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
