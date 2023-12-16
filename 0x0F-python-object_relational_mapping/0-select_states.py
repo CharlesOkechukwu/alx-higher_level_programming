64b3e64813db3a143283f41f2884e651259a50f1
CharlesOkechukwu <realcharlieok@gmail.com>
2023-12-16 04:07:36 -0800

    list all states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""list all states in a database module"""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    """print out all states stored in
+    a database module
+    """
+   conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1], password=argv[2], db=argv[3], charset="utf8")
+   cur = conn.cursor()
+   cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
+   rows = cur.fetchall()
+   for row in rows:
+       print(row)
