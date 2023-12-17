da4f627dfbe8653b1dfd9a1123cbee7ff4b59ef6
CharlesOkechukwu <realcharlieok@gmail.com>
2023-12-17 03:46:17 -0800

    print first result

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""module to select all states"""
+from model_state import Base, State
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    """select all states using sql alchemy
+    must not execute when imported
+    """
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2], argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).order_by(State.id).first()
+    if state is None:
+        print('Nothing')
+    else:
+        print("{}: {}".format(state.id, state.name))
