213e86e9ad5f58a316e37ae4793229a4db562fa8
CharlesOkechukwu <realcharlieok@gmail.com>
2023-12-17 06:35:34 -0800

    update state name

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""module to select all states"""
+from model_state import Base, State
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    """select all states using sql alchemy
+    must not execute when imported
+    """
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2], argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).filter(State.id == '2').first()
+    state.name = 'New Mexico'
+    session.commit()
+    session.close()
