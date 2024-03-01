ad20c1d68bd26f36a014fc2dfa18e2c5f215863d
CharlesOkechukwu <realcharlieok@gmail.com>
2024-03-01 11:42:44 -0800

    fetch name from api using jquery"

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,7 @@
+const url = 'https://swapi-api.alx-tools.com/api/people/5/?format=json'
+$.get(url, { format: 'json' }, function (data, stat) {
+  if (stat === 'success') {
+    const name = data.name;
+    $('DIV#character').text(name);
+  }
+});
